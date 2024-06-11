4ad71f06002594fd7cdfa9b8bce768773edaf8fc
xwang049 <xwang049@e.ntu.edu.sg>
2024-06-11 20:02:09 +0800

    0611

---
+++ README.md
@@ -0,0 +1,10 @@
+Create .env file in the root folder
+
+Example:
+```
+NASDAQ_API = '***'
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = '***'
+MYSQL_DB = 'info'
+```
---
+++ main.py
@@ -0,0 +1,34 @@
+
+import pandas as pd
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+import nasdaqdatalink as nd
+nd.ApiConfig.api_key = "vMy2zfhwoANpKo1Y9PYV"
+
+# metadata = nd.get('NSE/OIL')
+# print(metadata)
+
+# data = nd.Dataset('WIKI/AAPL').data()
+# dataset_data = nd.Dataset('WIKI/AAPL').data(params={ 'start_date':'2001-01-01', 'end_date':'2010-01-01', 'collapse':'annual', 'transformation':'rdiff', 'rows':4 })
+# print(dataset_data[0].date)
+
+
+# data = nd.Datatable('ZACKS/FC').data()
+# data2 = nd.Datatable('ZACKS/FC').data(params={'qopts': {'cursor_id': data.meta['next_cursor_id']}})
+#
+# data_list = []
+# cursor_id = None
+# while True:
+#     data = nd.Datatable('ZACKS/FC').data(params={'ticker': ['AAPL','MSFT'], 'per_end_date': {'gte': '2015-01-01'}, 'qopts': {'columns': ['ticker', 'comp_name'], 'cursor_id': cursor_id}})
+#     cursor_id = data.meta['next_cursor_id']
+#     data_list.append(data)
+#     if cursor_id is None:
+#         break
+# nd.Database('ZEA').bulk_download_url()
+
+
+db = nd.Database.all()
+print(db)
