3b897adc475964eb2abadfe90307469597b20784
Merrermer <merrermer66@gmail.com>
2024-06-21 11:59:21 +0800

    Update update.py

--- src/update.py
+++ src/update.py
@@ -2,12 +2,14 @@ import nasdaqdatalink as nd
 from datetime import datetime
 from pytz import timezone
 from prefect import task, flow
-import db
+import utils
+import download_data
 import pandas as pd
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 from datetime import datetime, timedelta
+from download_data import download_data
 
 load_dotenv()
 
@@ -35,26 +37,25 @@ def find_new_rows(existing_data, new_data):
 def insert_new_rows(engine, table_name, new_rows):
     new_rows.to_sql(table_name, engine, if_exists='append', index=False)
 
-@flow
 def get_latest_data(table_code):
     edt = timezone('US/Eastern')
     current_time_edt = datetime.now(edt)
     previous_day = current_time_edt - timedelta(days=2)
     date_str = previous_day.strftime('%Y-%m-%d')
     try:
-        table, data = db.download_data.submit('NDAQ/RTAT10', paginate = True,  **{'date': {'gte': date_str}}).result()
+        table, data = download_data.submit('NDAQ/RTAT10', paginate = True,  **{'date': {'gte': date_str}}).result() # download directly from column 'date' 
     except:
         try:
             print('try1')
             df = nd.get_table(table_code).head()
             date_str = current_time_edt.strftime('%Y-%m-%d')
             date_guess = [col for col in df.columns if 'date' in col.lower()][0]
-            table, data = db.download_data.submit(table_code, paginate = True,  **{date_guess: {'gte': date_str}}).result()
+            table, data = download_data.submit(table_code, paginate = True,  **{date_guess: {'gte': date_str}}).result() # In case if 'date' does not exist, we guess a filter that represents datetime
         except:
             try:
                 print('try2')
                 data = nd.get_table(table_code)
-                existing_data = get_existing_data(engine, table_code.replace('/', '_').lower())
+                existing_data = get_existing_data(engine, table_code.replace('/', '_').lower()) # other cases, we simply download the latest 10000 rows of data and insert the new rows by comparing them with the previous data
                 data = find_new_rows(existing_data, data)
             except:
                 return None
